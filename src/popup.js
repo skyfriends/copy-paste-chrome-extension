c1ba5765f6b04316fd34c1e7b4a93ef2b61bc320
Dustin Byers <dbyers84@gmail.com>
2017-11-20 13:19:49 -0800

    things

--- src/popup.js
+++ src/popup.js
@@ -1,42 +1,46 @@
-// gearform -> https://www.gearbubble.com/dropship_order/shipping_info
+// // gearform -> https://www.gearbubble.com/dropship_order/shipping_info
 
-//** Gearbubble.com ids **/
-let firstName = 'dropship_order_first_name'
-let lastName = 'dropship_order_last_name'
-let address1 = 'dropship_order_address1'
-let address2 = 'dropship_order_address2'
-let city = 'dropship_order_city'
-let zip = 'dropship_order_zip_code'
+// //** Gearbubble.com ids **/
+// let firstName = 'dropship_order_first_name'
+// let lastName = 'dropship_order_last_name'
+// let address1 = 'dropship_order_address1'
+// let address2 = 'dropship_order_address2'
+// let city = 'dropship_order_city'
+// let zip = 'dropship_order_zip_code'
 
-// dropdown for country selector, <option value by iso2 country code
-let countryDropdown = 'dropship_order_country'
-// dropdown for state selection, M <option value = 'WA', 'OR'>, etc\
-let stateDropdown = 'state_select'
-let orderNumber = 'dropship_order_order_number'
-let orderId = 'dropship_order_order_id'
+// // dropdown for country selector, <option value by iso2 country code
+// let countryDropdown = 'dropship_order_country'
+// // dropdown for state selection, M <option value = 'WA', 'OR'>, etc\
+// let stateDropdown = 'state_select'
+// let orderNumber = 'dropship_order_order_number'
+// let orderId = 'dropship_order_order_id'
 
-// current website return
-let page = window.location.href.includes('gearbubble.com') ? 'gearBubble' : 'gooten';
+// // current website return
+// let page = window.location.href.includes('gearbubble.com') ? 'gearBubble' : 'gooten';
 
-// array of ids to reference corresponding input
-let idArr = [firstName, lastName, address1, address2, city, zip, countryDropdown, stateDropdown, orderNumber, orderId];
+// // array of ids to reference corresponding input
+// // current website return
+// let page = window.location.href.includes('gearbubble.com') ? 'gearBubble' : 'gooten';
 
-// future array for input values to be pasted
-let copyVals = [firstNameCopyVal, lastNameCopyVal, address1CopyVal, address2CopyVal, cityCopyVal, zipCopyVal, 
-  countryDropdownCopyVal, stateDropdownCopyVal, orderNumberCopyVal, orderIdCopyVal]
 
-// populate inputs with copy data 
-// function populateInputs() {
-//   if page === 'gearBubble'
-//     idArr.map((id, index) => (document.getElementById(id).value = copyVals[index]))
+document.getElementById('clickme').addEventListener('click', runSwitchjs)
+
+// // future array for input values to be pasted
+// let copyVals = [firstNameCopyVal, lastNameCopyVal, address1CopyVal, address2CopyVal, cityCopyVal, zipCopyVal, 
+//   countryDropdownCopyVal, stateDropdownCopyVal, orderNumberCopyVal, orderIdCopyVal]
+
+// // populate inputs with copy data 
+// // function populateInputs() {
+// //   if page === 'gearBubble'
+// //     idArr.map((id, index) => (document.getElementById(id).value = copyVals[index]))
+// // }
+
+// function click(e) {
+//   chrome.tabs.executeScript(null, {
+//     code: `document.body.style.backgroundColor='${e.target.id}'`,
+//   });
+//   window.close();
 // }
-
-function click(e) {
-  chrome.tabs.executeScript(null, {
-    code: `document.body.style.backgroundColor='${e.target.id}'`,
-  });
-  window.close();
-}
 
 window.addEventListener('copy', (e) => {
   const stored = [];
@@ -54,9 +58,15 @@ window.addEventListener('copy', (e) => {
   e.preventDefault();
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const divs = document.querySelectorAll('div');
-  for (let i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
-  }
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   const divs = document.querySelectorAll('div');
+//   for (let i = 0; i < divs.length; i++) {
+//     divs[i].addEventListener('click', click);
+//   }
+// }page);
+
+function runSwitchjs() {
+  chrome.tabs.executeScript({
+    file: 'switch.js'
+  })
+}
